6c07948eac01479f6f7a3e4cde5693ff8b865763
Sylviane Farooq <sylvianepinto@hotmail.fr>
2023-03-10 16:13:59 +0100

    header & banner responsive fix

--- src/Layouts/Header.jsx
+++ src/Layouts/Header.jsx
@@ -1,13 +1,19 @@
 import { Link, NavLink } from "react-router-dom";
-import logo from "../assets/logo.png";
+import logoMobile from "../assets/logo_mb.png"
+import logo from "../assets/logo_dsk.png";
+
 
 function Header() {
+  const dskLogo = `${logo} 145w`
+  const mbLogo = `${logoMobile} 211w`
   return (
     <header className="header">
       <Link to="/">
-        <div className="header__logo-container">
-        <img className="header__logo" src={logo} alt="Kasa logo" />
-        </div>
+        <picture className="header__logo-container">
+          <source media="(max-width: 425px)" srcSet={mbLogo} sizes="425px" />
+          <source media="(min-width: 768px)" srcSet={dskLogo} sizes="768px" />
+          <img className="header__logo" src={logo} alt="Kasa logo" />
+        </picture>
       </Link>
       <nav className="header__nav">
         <NavLink className="header__nav-link" to="/">
